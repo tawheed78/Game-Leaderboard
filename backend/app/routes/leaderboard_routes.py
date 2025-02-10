71bb31502fb1950d9f7e4a76b80fc8be743f5fa1
Adhishri Karbhari <tawheed.chilwan55@gmail.com>
2025-02-11 03:39:11 +0530

    created game leaderboard service and separated route and business logic

--- backend/app/routes/leaderboard_routes.py
+++ backend/app/routes/leaderboard_routes.py
@@ -3,14 +3,13 @@ import redis.asyncio as aioredis # type: ignore
 
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from sqlalchemy.exc import SQLAlchemyError
-from redis.exceptions import RedisError # type: ignore
 
 from ..schemas.postgres_schema import LeaderboardResponse
 from ..configs.redis.redis import get_redis_client
 from ..configs.database.postgres_config import get_postgres_db
 from ..models.postgres_models import GameModel
 from ..services.leaderboard_service import global_leaderboard_service, game_leaderboard_service
+
 router = APIRouter()
 
 @router.get("/leaderboard/global", response_model=List[LeaderboardResponse])
