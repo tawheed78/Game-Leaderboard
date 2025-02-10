6ab17015763b57da03ce4fe87a60188ebdb6b10b
Adhishri Karbhari <tawheed.chilwan55@gmail.com>
2025-02-11 02:46:36 +0530

    created user service and separated route and business logic

--- backend/app/routes/user_routes.py
+++ backend/app/routes/user_routes.py
@@ -1,62 +1,35 @@
-from datetime import datetime
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from sqlalchemy.exc import SQLAlchemyError
 
 from ..models.postgres_models import UserModel
 from ..schemas.postgres_schema import UserCreate, UserUpdate, User
 from ..configs.database.postgres_config import get_postgres_db
+from ..services.user_service import user_create_service, user_update_service, user_delete_service
 
 router = APIRouter()
 
 @router.post("/users", response_model=User)
 async def create_user(user: UserCreate, db: Session = Depends(get_postgres_db)):
-    try:
-        existing_user = db.query(UserModel).filter(UserModel.email == user.email).first()
-        if existing_user:
-            raise HTTPException(status_code=400, detail="Email already registered")
-        db_user = UserModel(
-            username=user.username,
-            email=user.email,
-            password=user.password,
-            created_at=user.created_at,
-            updated_at=user.updated_at
-        )
-        db.add(db_user)
-        db.commit()
-        db.refresh(db_user)
-        return db_user
-    except SQLAlchemyError as e:
-        db.rollback()
-        raise HTTPException(status_code=400, detail=f"Database Error: {str(e)}")
+    existing_user = db.query(UserModel).filter(UserModel.email == user.email).first()
+    if existing_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    response = await user_create_service(user, db)
+    return response
     
 
 @router.put("/users/{user_id}", response_model=User)
 async def update_user(user_id: int, user: UserUpdate, db: Session = Depends(get_postgres_db)):
-    try:
-        db_user = db.query(UserModel).filter(UserModel.id == user_id).first()
-        if db_user is None:
-            raise HTTPException(status_code=404, detail="User not found")
-        db_user.username = user.username or db_user.username
-        db_user.email = user.email or db_user.email
-        db_user.password = user.password or db_user.password
-        db_user.updated_at = datetime.now()
-        db.commit()
-        db.refresh(db_user)
-        return db_user
-    except SQLAlchemyError as e:
-        db.rollback()
-        raise HTTPException(status_code=400, detail=f"Database Error: {str(e)}")
+    db_user = db.query(UserModel).filter(UserModel.id == user_id).first()
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    response = await user_update_service(db_user, user, db)
+    return response
     
-@router.delete("users/{user_id}")
+
+@router.delete("/users/{user_id}")
 async def delete_user(user_id: int, db: Session = Depends(get_postgres_db)):
-    try:
-        db_user = db.query(UserModel).filter(UserModel.id == user_id).first()
-        if db_user is None:
-            raise HTTPException(status_code=404, detail="User not found")
-        db.delete(db_user)
-        db.commit()
-        return {"message": "User deleted successfully"}
-    except SQLAlchemyError as e:
-        db.rollback()
-        raise HTTPException(status_code=400, detail=f"Database Error: {str(e)}")
+    db_user = db.query(UserModel).filter(UserModel.id == user_id).first()
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    response = await user_delete_service(db_user, db)
+    return response
---
+++ backend/app/services/user_service.py
@@ -0,0 +1,55 @@
+
+from datetime import datetime
+from fastapi import HTTPException
+from fastapi.responses import JSONResponse
+from sqlalchemy.exc import SQLAlchemyError
+from ..schemas.postgres_schema import UserCreate, UserUpdate
+from ..models.postgres_models import UserModel
+
+
+async def user_create_service(user: UserCreate, db):
+    try:
+        db_user = UserModel(
+            username=user.username,
+            email=user.email,
+            password=user.password,
+            created_at=user.created_at,
+            updated_at=user.updated_at
+        )
+        db.add(db_user)
+        db.commit()
+        db.refresh(db_user)
+        return db_user
+    except SQLAlchemyError as e:
+        db.rollback()
+        return JSONResponse(status_code=400,content=f"Database Error: {str(e)}")
+    except Exception as e:
+        return JSONResponse(status_code=500, content=f"Unexpected Error: {str(e)}")
+
+
+async def user_update_service(db_user, user: UserUpdate, db):
+    try:
+        db_user.username = user.username or db_user.username
+        db_user.email = user.email or db_user.email
+        db_user.password = user.password or db_user.password
+        db_user.updated_at = datetime.now()
+        db.commit()
+        db.refresh(db_user)
+        return db_user
+    except SQLAlchemyError as e:
+        db.rollback()
+        raise HTTPException(status_code=400, detail=f"Database Error: {str(e)}")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Unexpected Error: {str(e)}")
+    
+
+async def user_delete_service(db_user, db):
+    try:
+        db.delete(db_user)
+        db.commit()
+        return {"message": "User deleted successfully"}
+    except SQLAlchemyError as e:
+        db.rollback()
+        raise HTTPException(status_code=400, detail=f"Database Error: {str(e)}")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Unexpected Error: {str(e)}")
